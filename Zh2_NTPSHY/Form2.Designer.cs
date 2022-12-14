982ce09fed473b082480fb1775f2d89df72cf77e
TheReif <reifmarton@gmail.com>
2022-12-14 14:46:26 +0100

    vegleges2

--- Zh2_NTPSHY/Form2.Designer.cs
+++ Zh2_NTPSHY/Form2.Designer.cs
@@ -57,7 +57,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Ok";
+            this.button1.Text = "Mentés";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- Zh2_NTPSHY/UserControl1.Designer.cs
+++ Zh2_NTPSHY/UserControl1.Designer.cs
@@ -41,6 +41,8 @@
             this.roomNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dtgBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.dtgBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.listBBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.timeBindingSource)).BeginInit();
@@ -146,10 +148,30 @@
             // 
             this.dtgBindingSource.DataSource = typeof(Zh2_NTPSHY.Dtg);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Termek:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Targyak:";
+            // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
@@ -182,5 +204,7 @@
         private DataGridViewTextBoxColumn roomNameDataGridViewTextBoxColumn;
         private BindingSource dtgBindingSource;
         private BindingSource dtgBindingSource1;
+        private Label label1;
+        private Label label2;
     }
 }
